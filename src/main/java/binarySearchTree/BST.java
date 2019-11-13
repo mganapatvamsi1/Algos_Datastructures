ad030daf8cbc945f9a2e12c7797277eae8325a19
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2019-11-12 20:27:45 -0600

    Create BST.java
    Representing BinarySearchTree

---
+++ src/main/java/binarySearchTree/BST.java
@@ -0,0 +1,19 @@
+package binarySearchTree;
+
+public class BST {
+    private N root;
+
+    private class N {
+        N left, right;
+        int data;
+
+        public N(int data) {
+            this.data = data;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
