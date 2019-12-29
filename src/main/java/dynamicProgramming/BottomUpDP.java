a0d891ce8df42c10d76f6489f110c3aae6c39161
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2019-12-29 16:50:38 -0600

    Dynamic Programming
    Bottom Up (Tabulation) Approach of Dynamic Programming

---
+++ src/main/java/dynamicProgramming/BottomUpDP.java
@@ -0,0 +1,20 @@
+package dynamicProgramming;
+
+public class BottomUpDP {
+
+    public static int fib(int n) {
+        int[] table = new int[n + 1];
+        table[0] = 0;
+        table[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            table[i] = table[i-1] + table[i-2];
+        }
+        return table[n];
+    }
+
+    public static void main(String[] args) {
+        BottomUpDP fibonacci = new BottomUpDP();
+        System.out.println(fibonacci.fib(7));
+    }
+
+}
